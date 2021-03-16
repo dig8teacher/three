22e9d9fd870ed574fcacfe1e25ef3b6fd7ee171b
dig8teacher <jgill@sd42.ca>
2021-03-15 22:07:30 -0700

    maggie9

---
+++ variableone.java
@@ -0,0 +1,8 @@
+class variableone {
+	public static void main(String[] args) 
+	{
+		int number1 = 12;
+		int number2 = 22;
+		System.out.println(number1+number2);
+	}
+}
---
+++ variabletwo.java
@@ -0,0 +1,8 @@
+class variabletwo {
+	public static void main(String[] args)
+ {
+	int number1 = 5;
+	int number2 = 5;
+	System.out.println("the answer is" + (number1 + number2));
+ }
+}
